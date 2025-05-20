0b3965c88d18f899f58968bf9c4979d97e94fa90
紫地丁 <hanfe666@outlook.com>
2025-05-20 20:56:17 +0800

    [feat] Virtual Machine

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include "chunk.h"
 #include "debug.h"
-
+#include"vm.h"
 int main(void) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
-    disassembleChunk(&chunk, "test chunk");
+    // disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
-    PRINT_SIZE(OpCode);
+    // PRINT_SIZE(OpCode);
     return 0;
 }

f6064c8f986e72574e1ce6ca6f8eed4abf7b10f0
紫地丁 <hanfe666@outlook.com>
2025-05-21 21:01:35 +0800

    [feat] Scanner

--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 
 #include "vm.h"
@@ -77,8 +78,7 @@ push(a op b); \
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
